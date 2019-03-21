ee22703a82f826154095c4728897dda28c90e87c
Eric Dong <eric.dongxx@gmail.com>
2019-03-21 10:06:28 +0800

    Initial commit of UR cobot related work.
    Signed-off-by: Eric Dong <eric.dongxx@gmail.com>

---
+++ src/ur5_test.cpp
@@ -0,0 +1,111 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the organization nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Eric Dong */
+#include <ros/ros.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "ur5_test");
+  ros::NodeHandle node_handle;
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  // Find the planning group of UR5 and set it up
+  static const std::string PLANNING_GROUP = "manipulator";
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  const robot_state::JointModelGroup* joint_model_group = 
+    move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // Getting basic information
+  ROS_INFO_NAMED("test", "Planning frame: %s", 
+      move_group.getPlanningFrame().c_str());
+
+  // Print the end effector
+  ROS_INFO_NAMED("test", "End effector link: %s",
+      move_group.getEndEffectorLink().c_str());
+
+  // Get a list of all the groups in UR5
+  ROS_INFO_NAMED("test", "Available Planning Groups:");
+  std::copy(move_group.getJointModelGroupNames().begin(),
+    move_group.getJointModelGroupNames().end(),
+    std::ostream_iterator<std::string>(std::cout, ", "));
+
+  // Planning to a pose goal for the end effector
+  geometry_msgs::Pose target_pose1;
+  target_pose1.orientation.w = 1.0;
+  target_pose1.position.x = 0.28;
+  target_pose1.position.y = -0.2;
+  target_pose1.position.z = 0.5;
+  move_group.setPoseTarget(target_pose1);
+  
+  // Next call the planner to compute the plan
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+  bool success = (move_group.plan(my_plan) == 
+      moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  ROS_INFO_NAMED("test", "plan 1 (pose goal) %s", success ? "" : 
+      "FAILED");
+
+  // Planning a joint-space goal
+  moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+
+  // Next get the current set of joint values for the group
+  std::vector<double> joint_group_positions;
+  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+
+  // Plan to the new joint space goal
+  joint_group_positions[0] = -1.0; // radians
+  move_group.setJointValueTarget(joint_group_positions);
+
+  success = (move_group.plan(my_plan) ==
+      moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("test", "plan 2 (joint space goal) %s", success ? "" : "FAILED");
+
+  // while(ros::ok())
+  // {
+  //   ros::spinOnce();
+  // }
+  ros::shutdown();
+  return 0;
+}
+
